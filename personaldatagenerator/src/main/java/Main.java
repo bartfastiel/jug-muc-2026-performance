60c3c0f9c1a51e4d9e10ab846a61e27e866f9160
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2026-01-14 20:45:34 +0100

    Write as zip file

--- personaldatagenerator/src/main/java/Main.java
+++ personaldatagenerator/src/main/java/Main.java
@@ -1,9 +1,14 @@
 
 void main() throws IOException {
     System.out.println("Personal Data Generator");
-    var outputFileName = "personal-data.txt";
+    var outputFileName = "personal-data.zip";
     var outputPath = Path.of(outputFileName);
-    try (var writer = Files.newBufferedWriter(outputPath)) {
+    try (
+            var zipOutputStream = new ZipOutputStream(Files.newOutputStream(outputPath));
+            var out = new OutputStreamWriter(zipOutputStream);
+            var writer = new BufferedWriter(out);
+    ) {
+        zipOutputStream.putNextEntry(new ZipEntry("personal-data.csv"));
         writer.write("First name;Last name ;\n");
         writer.write("Max       ;Mustermann;\n");
     }

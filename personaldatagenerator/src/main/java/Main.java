0a490713aea72f0dfef6c281dc876fa31c797109
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2026-01-14 20:50:26 +0100

    Write 100 lines with random first names

--- personaldatagenerator/src/main/java/Main.java
+++ personaldatagenerator/src/main/java/Main.java
@@ -1,4 +1,8 @@
 
+static final String[] FIRST_NAMES = {
+        "Max", "Moritz", "Susi", "Lieschen", "Hans", "Greta"
+};
+
 void main() throws IOException {
     System.out.println("Personal Data Generator");
     var outputFileName = "personal-data.zip";
@@ -10,6 +14,11 @@ void main() throws IOException {
     ) {
         zipOutputStream.putNextEntry(new ZipEntry("personal-data.csv"));
         writer.write("First name;Last name ;\n");
-        writer.write("Max       ;Mustermann;\n");
+        var random = new Random();
+        for (int i = 0; i < 100; i++) {
+            var firstName = FIRST_NAMES[random.nextInt(FIRST_NAMES.length)];
+            var lastName = "LastName" + (i + 1);
+            writer.write(firstName + ";" + lastName + ";\n");
+        }
     }
 }

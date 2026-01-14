6d153c9b0df680931e138643451d3d3baa1ba512
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2026-01-14 20:42:50 +0100

    Write Max Mustermann to file

--- personaldatagenerator/src/main/java/Main.java
+++ personaldatagenerator/src/main/java/Main.java
@@ -1,4 +1,10 @@
 
-void main() {
-    IO.println(String.format("Hello and welcome!"));
+void main() throws IOException {
+    System.out.println("Personal Data Generator");
+    var outputFileName = "personal-data.txt";
+    var outputPath = Path.of(outputFileName);
+    try (var writer = Files.newBufferedWriter(outputPath)) {
+        writer.write("First name;Last name ;\n");
+        writer.write("Max       ;Mustermann;\n");
+    }
 }

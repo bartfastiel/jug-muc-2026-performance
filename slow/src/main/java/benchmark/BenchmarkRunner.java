4b1cebc077414342f3466cb6d06ec420fb314ff6
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2026-02-07 11:58:12 +0100

    Always generate input before benchmarking

--- slow/src/main/java/benchmark/BenchmarkRunner.java
+++ slow/src/main/java/benchmark/BenchmarkRunner.java
@@ -1,15 +1,18 @@
 package benchmark;
 
+import generator.PersonalDataGenerator;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class BenchmarkRunner {
 
     static void main() throws Exception {
+        var persons = 500;
+        PersonalDataGenerator.generate(persons);
         new Runner(
                 new OptionsBuilder()
                         .include(BenchmarkConfig.class.getName().replace(".", "\\."))
-                        .param("numberOfPersons", String.valueOf(5_000_000))
+                        .param("numberOfPersons", String.valueOf(persons))
                         .build()
         ).run();
     }
